ff8cff112314ea7415d66342bb85789acc8b30f6
k-kazuma <k_kazuma8@icloud.com>
2023-03-16 23:56:09 +0900

    ヘッダー更新

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "the-new-css-reset/css/reset.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/style.css'
 
 import type { AppProps } from 'next/app'
 
--- pages/header.tsx
+++ pages/header.tsx
@@ -1,30 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { ToggleButton } from 'react-bootstrap'
 
 function header() {
+
+  const [toggleButton, setToggleButton] = useState(false);
+
   return (
-    <header className='bg-secondary mt-0'>
-      <div className='d-flex justify-content-between container mt-0'>
-        <Link href={"/"} className="text-decoration-none">
-          <div className='fs-2 text-light'>web制作Monkye</div>
-        </Link>
-        <div>
-          <nav className='mt-3'>
-            <ul className='d-flex mb-0'>
-              <Link href={"/service"} className="text-decoration-none">
-                <li className='me-3 text-light'>サービス</li>
-              </Link>
-              <Link href={"/profile"} className="text-decoration-none">
-                <li className='me-3 text-light'>プロフィール</li>
-              </Link>
-              <Link href={"/form"} className="text-decoration-none">
-                <li className='text-light'>問い合わせ</li>
-              </Link>
-            </ul>
-          </nav>
+    <>
+      <header className='bg-secondary mt-0 pt-2 pb1 position-relative'>
+        <div className='d-flex justify-content-between container mt-0'>
+          <Link href={"/"} className="text-decoration-none">
+            <div className='fs-2 text-light'>web制作Monkye</div>
+          </Link>
+          <div>
+            <nav className='mt-3 d-none d-sm-block'>
+              <ul className='d-flex mb-0'>
+                <Link href={"/service"} className="text-decoration-none">
+                  <li className='me-3 text-light'>サービス</li>
+                </Link>
+                <Link href={"/profile"} className="text-decoration-none">
+                  <li className='me-3 text-light'>プロフィール</li>
+                </Link>
+                <Link href={"/form"} className="text-decoration-none">
+                  <li className='text-light'>問い合わせ</li>
+                </Link>
+              </ul>
+            </nav>
+            <div className='d-sm-none'>
+              <button 
+              className={`menuButton ${toggleButton && "toggleButton"}`}
+              onClick={() => {setToggleButton(!toggleButton);}}
+              >
+                <span></span>
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
       </header>
+      <div className={`headerMenu ${toggleButton && "toggleButton"}`}>
+          <nav className='text-center'>
+              <ul className='mt-5'>
+                <Link href={"/"} className="text-decoration-none">
+                  <li className='text-light py-2 fs-3'>トップ</li>
+                </Link>
+                <Link href={"/service"} className="text-decoration-none">
+                  <li className='text-light py-2 fs-3'>サービス</li>
+                </Link>
+                <Link href={"/profile"} className="text-decoration-none">
+                  <li className='text-light py-2 fs-3'>プロフィール</li>
+                </Link>
+                <Link href={"/form"} className="text-decoration-none">
+                  <li className='text-light py-2 fs-3 '>問い合わせ</li>
+                </Link>
+              </ul>
+            </nav>
+      </div>
+    </>
   )
 }
 
