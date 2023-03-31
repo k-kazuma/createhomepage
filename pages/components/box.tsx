7f305624a88f18085165f25b4fd2fe2a8eae80b0
k-kazuma <k_kazuma8@icloud.com>
2023-04-01 07:52:39 +0900

    ビルドエラー解消（useFrame）

--- pages/components/box.tsx
+++ pages/components/box.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import Cube from './cube'
-import Cube2 from './cube2'
+import React, {useRef, useState} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import type { Mesh } from 'three'
+
+function Cube({position=[0, 0, 0]}: {position?:number[]}) {
+
+  const cubeRef = useRef<Mesh>(null)
+  let [translate, setTranslate] = useState(position[1])
+  useFrame(() => {
+      const cube = cubeRef.current;
+      if(!cube) return;
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      setTranslate(translate + 0.01)
+      if(translate > 6.2) setTranslate(-5)
+  });
+
+  return(
+      <mesh ref={cubeRef} position={[position[0], translate, position[2]]}>
+          <boxBufferGeometry args={[0.7,0.7, 0.7]}/>
+          <meshStandardMaterial 
+          color="white" 
+          />
+          
+      </mesh>
+  )
+}
+
+function Cube2({position=[0, 0, 0]}: {position?:number[]}) {
+
+  const cubeRef2 = useRef<Mesh>(null)
+      
+      useFrame(() => {
+          const cube = cubeRef2.current;
+          if(!cube) return;
+          cube.rotation.y += 0.003;
+
+      });
+
+  
+
+  return(
+      <mesh ref={cubeRef2} position={[position[0], position[1], position[2]]}>
+          <boxBufferGeometry args={[1.9, 1.9, 1.9]}/>
+          <meshStandardMaterial 
+          color="white" 
+          />
+          
+      </mesh>
+  )
+}
 
 function Box() {
   return (
