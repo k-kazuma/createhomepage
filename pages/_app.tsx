b6f3bf1798f09c846b0bdfbe9d55107544420515
k-kazuma <k_kazuma8@icloud.com>
2023-04-13 21:16:45 +0900

    ロード画面修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/style.scss';
 
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import Load from './components/load';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [load, setLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(true);
+    }, 4000);
+  });
+
+  return !load ? <Load load={load} /> : <Component {...pageProps} load={load} setLoad={setLoad} />;
 }
